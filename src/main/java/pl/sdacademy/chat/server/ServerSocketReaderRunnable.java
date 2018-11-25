d2c201484561c49518079cf6f61628c5d2e0e407
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-25 14:26:06 +0100

    - ServerSocketReaderRunnable.java added

---
+++ src/main/java/pl/sdacademy/chat/server/ServerSocketReaderRunnable.java
@@ -0,0 +1,50 @@
+package pl.sdacademy.chat.server;
+
+import pl.sdacademy.chat.model.ChatMessage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+public class ServerSocketReaderRunnable implements Runnable {
+    private final Socket client;
+    private final ChatLog chatLog;
+
+    public ServerSocketReaderRunnable(Socket client, ChatLog chatLog) {
+        this.client = client;
+        this.chatLog = chatLog;
+    }
+
+    @Override
+    public void run() {
+        // zarejestruj klienta w chatlogu
+        // jeżeli się udało, to pobierz ObjectInputStream dla tego klienta (try-with-resources)
+        // w pętli odczytujemy komunikaty od klienta tak długo, aż pojawi się "exit"
+        // komunikat przekaż do ChatLogu, ale nie przekazuj komunikaty "exit"
+        // jeżeli pojawił się exit, lub nie udało się odczytać komunikatu od klienta
+        // to wyrejestruj się z chatlogu i zakończ to zadanie
+
+        if (!chatLog.register(client)) { // fail fast
+            return;
+        }
+        try (ObjectInputStream clientInput = new ObjectInputStream(client.getInputStream())) {
+            processClientInput(clientInput);
+        } catch (IOException e) {
+            System.out.println("### Client disconnected due to network problem ###");
+        } catch (ClassNotFoundException e) {
+            System.out.println("### Client disconnected due to invalid message format ###");
+        }
+        chatLog.unregister(client);
+    }
+
+    private void processClientInput(ObjectInputStream clientInput) throws IOException, ClassNotFoundException {
+        while (true) {
+            ChatMessage chatMessage = (ChatMessage) clientInput.readObject();
+            if (chatMessage.getMessage() == null ||
+                    chatMessage.getMessage().equalsIgnoreCase("exit")) {
+                break;
+            }
+            chatLog.acceptMessage(chatMessage);
+        }
+    }
+}
